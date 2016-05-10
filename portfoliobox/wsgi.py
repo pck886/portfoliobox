d6b89f729e21ddc2adf8f1c90e6e86171f88ae09
chanlee <pck886@gmail.com>
2016-05-10 09:32:38 +0900

    -

--- portfoliobox/wsgi.py
+++ portfoliobox/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
+import django
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfoliobox.settings")
@@ -13,5 +14,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfoliobox.settings")
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+application = django.core.handlers.wsgi.WSGIHandler()
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
